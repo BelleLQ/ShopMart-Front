a44721e7f9ae36eb3e3a070bfad3b16c48e77b20
BelleLQ <59607658+BelleLQ@users.noreply.github.com>
2021-11-15 01:40:04 -0500

    enlarged the img when hover

--- src/components/ProdCatItem.js
+++ src/components/ProdCatItem.js
@@ -6,7 +6,7 @@ import{Link} from 'react-router-dom'
 const ProdCatItem = (props) => {
     return (
         <div className = "item col-sm-6 col-lg-4 text-center">
-            <Card>
+            <Card style={{overflow: 'hidden'}}>
                 <Card.Img variant="top" src={props.data.photoUrl} style={{height: '190px', 'objectFit':'cover','min-width':'100%'}}/>
                 <Card.Body>
                     <Card.Title>{props.data.categoryName}</Card.Title>
